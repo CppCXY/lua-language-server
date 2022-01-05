7e6cd5ff0131983e0cd882bed8629d1d775852be
CppCXY <812125110@qq.com>
2022-01-05 23:45:40 +0800

    取消改动

--- script/proto/define.lua
+++ script/proto/define.lua
@@ -58,7 +58,6 @@ m.DiagnosticDefaultSeverity = {
     ['doc-field-no-class']      = 'Warning',
     ['duplicate-doc-field']     = 'Warning',
     ['unknown-diag-code']       = 'Waiting',
-    ['unimport-module']         = 'Hint'
 }
 
 ---@alias DiagnosticDefaultNeededFileStatus
